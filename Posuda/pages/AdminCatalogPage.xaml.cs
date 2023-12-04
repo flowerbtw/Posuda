c704711656a36912d5ab4df59e936cc45c8e2c32
Artem Melnikov <meln1kbtw@gmail.com>
2023-12-04 14:57:26 +0300

    1

--- Posuda/pages/AdminCatalogPage.xaml.cs
+++ Posuda/pages/AdminCatalogPage.xaml.cs
@@ -13,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Posuda
+namespace Posuda.pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для AdminCatalogPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AdminCatalogPage : Page
     {
-        public MainWindow()
+        public AdminCatalogPage()
         {
             InitializeComponent();
-            MainFrame.Navigate(new LoginPage());
         }
     }
 }
